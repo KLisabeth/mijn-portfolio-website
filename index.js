7b3d3027f6e04c0fd22ac6380961d493fc9c0c1a
Katie <katie_lisabeth@yahoo.com>
2021-04-01 15:05:47 +0200

    add mongo db
    and download `nodemon`

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
+
+//Mongo Db
+const db = process.env.MDB_URL;
+mongoose
+  .connect(db, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.log(error.reason));
+//
 
 const port = process.env.PORT;
 
