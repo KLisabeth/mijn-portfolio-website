4280c7ba565287fb560ec66e9f700bf36f93148c
Katie <katie_lisabeth@yahoo.com>
2021-04-02 15:16:29 +0200

    create home page and about me content
    implement bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
     <Header/>
+<Route to="/" exact={true} component={Home}/>
     <Footer/>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from './About'
+
+function Home() {
+    return (
+        <div>
+      <About/>
+        </div>
+    )
+}
+
+export default Home
+
